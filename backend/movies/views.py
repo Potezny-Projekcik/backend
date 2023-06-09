705bb04b26d693233482069f05a72d801f8c12f6
root <root@DESKTOP-B2J9GB2>
2023-06-09 12:13:29 +0200

    Added usermoviesbycategory view

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,6 +1,11 @@
 from rest_framework import viewsets
 from .models import Category, Director, Directors, Language, Languages, Movie, Moviecategory, Producer, Producers, User, Usermovies
 from .serializers import CategorySerializer, DirectorSerializer, DirectorsSerializer, LanguageSerializer, LanguagesSerializer, MovieSerializer, MovieCategorySerializer, ProducerSerializer, ProducersSerializer, UserSerializer, UserMoviesSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.db import connection
+import psycopg2
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -53,5 +58,33 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class UserMoviesViewSet(viewsets.ModelViewSet):
+
+
     queryset = Usermovies.objects.all()
     serializer_class = UserMoviesSerializer
+    @action(detail=False, methods=['get'], url_path='get-movies-by-category/(?P<category_id>[^/.]+)')
+    def get_usermovies_by_category(self, request, category_id):
+        query = """
+            SELECT DISTINCT ON ("UserMovies".usermovieid)
+            "UserMovies".*, "Movie".title, "Movie".genre, "Movie".countryoforigin, "Movie".productionyear, "Movie".suggestedage,
+            "Producer".producername, "Director".directorfirstname, "Director".directorlastname, "Language".languagename
+            FROM "UserMovies"
+            INNER JOIN "MovieCategory" ON "UserMovies".usermovieid = "MovieCategory".usermovieid
+            INNER JOIN "Movie" ON "UserMovies".movieid = "Movie".movieid
+            LEFT JOIN "Producers" ON "UserMovies".movieid = "Producers".movieid
+            LEFT JOIN "Producer" ON "Producers".producerid = "Producer".producerid
+            LEFT JOIN "Directors" ON "UserMovies".movieid = "Directors".movieid
+            LEFT JOIN "Director" ON "Directors".directorid = "Director".directorid
+            LEFT JOIN "Languages" ON "UserMovies".movieid = "Languages".movieid
+            LEFT JOIN "Language" ON "Languages".languageid = "Language".languageid
+            WHERE  "MovieCategory".categoryid=
+        """
+        secondQuery=f"{query}{category_id}"
+        print(secondQuery)
+
+        # Execute the raw query
+        with connection.cursor() as cursor:
+            cursor.execute(secondQuery)
+            results = cursor.fetchall()
+
+        return Response(results)
---
+++ baza/sql_script.sql
@@ -0,0 +1,279 @@
+-- Schema: mydb
+DROP DATABASE IF EXISTS "Baza1";
+CREATE DATABASE "Baza1";
+
+-- Table: User
+CREATE TABLE IF NOT EXISTS "User" (
+  "userid" SERIAL PRIMARY KEY,
+  "firstname" VARCHAR(45) NOT NULL,
+  "lastname" VARCHAR(45) NOT NULL,
+  "login" VARCHAR(45) NOT NULL,
+  "password" CHAR(32) NOT NULL,
+  "birthdate" DATE NOT NULL,
+  "isadmin" BOOLEAN NOT NULL DEFAULT false
+);
+
+-- Table: Category
+CREATE TABLE IF NOT EXISTS "Category" (
+  "categoryid" SERIAL PRIMARY KEY,
+  "categoryname" VARCHAR(45) NOT NULL
+);
+
+-- Table: Producer
+CREATE TABLE IF NOT EXISTS "Producer" (
+  "producerid" SERIAL PRIMARY KEY,
+  "producername" VARCHAR(45)
+);
+
+-- Table: Director
+CREATE TABLE IF NOT EXISTS "Director" (
+  "directorid" SERIAL PRIMARY KEY,
+  "directorfirstname" VARCHAR(45),
+  "directorlastname" VARCHAR(45)
+);
+
+-- Table: Language
+CREATE TABLE IF NOT EXISTS "Language" (
+  "languageid" SERIAL PRIMARY KEY,
+  "languagename" VARCHAR(45)
+);
+
+-- Table: Movie
+CREATE TABLE IF NOT EXISTS "Movie" (
+  "movieid" SERIAL PRIMARY KEY,
+  "title" VARCHAR(45) NOT NULL,
+  "genre" VARCHAR(45) NOT NULL,
+  "countryoforigin" VARCHAR(45) NOT NULL,
+  "productionyear" DATE NOT NULL,
+  "suggestedage" VARCHAR(2)
+);
+
+-- Table: Producers
+CREATE TABLE IF NOT EXISTS "Producers" (
+  "producersid" SERIAL PRIMARY KEY,
+  "producerid" INT NOT NULL,
+  "movieid" INT NOT NULL,
+  CONSTRAINT fk_producers_producer
+    FOREIGN KEY ("producerid")
+    REFERENCES "Producer" ("producerid"),
+  CONSTRAINT fk_producers_movie
+    FOREIGN KEY ("movieid")
+    REFERENCES "Movie" ("movieid")
+);
+
+-- Table: Directors
+CREATE TABLE IF NOT EXISTS "Directors" (
+  "directorsid" SERIAL PRIMARY KEY,
+  "directorid" INT NOT NULL,
+  "movieid" INT NOT NULL,
+  CONSTRAINT fk_directors_director
+    FOREIGN KEY ("directorid")
+    REFERENCES "Director" ("directorid"),
+  CONSTRAINT fk_directors_movie
+    FOREIGN KEY ("movieid")
+    REFERENCES "Movie" ("movieid")
+);
+
+-- Table: Languages
+CREATE TABLE IF NOT EXISTS "Languages" (
+  "languagesid" SERIAL PRIMARY KEY,
+  "languageid" INT NOT NULL,
+  "movieid" INT NOT NULL,
+  CONSTRAINT fk_languages_language
+    FOREIGN KEY ("languageid")
+    REFERENCES "Language" ("languageid"),
+  CONSTRAINT fk_languages_movie
+    FOREIGN KEY ("movieid")
+    REFERENCES "Movie" ("movieid")
+);
+
+
+-- Table: UserMovies
+CREATE TABLE IF NOT EXISTS "UserMovies" (
+  "userid" INT NOT NULL,
+  "movieid" INT NOT NULL,
+  "usermovieid" SERIAL PRIMARY KEY,
+  "sessiondate" DATE,
+  "sessiontime" TIME,
+  "sessionpriority" SMALLINT,
+  CONSTRAINT fk_user
+    FOREIGN KEY ("userid")
+    REFERENCES "User" ("userid"),
+  CONSTRAINT fk_movie
+    FOREIGN KEY ("movieid")
+    REFERENCES "Movie" ("movieid")
+);
+
+-- Table: MovieCategory
+CREATE TABLE IF NOT EXISTS "MovieCategory" (
+  "moviecategoryid" SERIAL PRIMARY KEY,
+  "usermovieid" INT NOT NULL,
+  "categoryid" INT NOT NULL,
+  CONSTRAINT fk_usermovies
+    FOREIGN KEY ("usermovieid")
+    REFERENCES "UserMovies" ("usermovieid"),
+  CONSTRAINT fk_category
+    FOREIGN KEY ("categoryid")
+    REFERENCES "Category" ("categoryid")
+);
+
+
+INSERT INTO "User" ("firstname", "lastname", "login", "password", "birthdate", "isadmin")
+VALUES
+    ('John', 'Doe', 'johndoe', 'password1', '1990-01-01', false),
+    ('Jane', 'Smith', 'janesmith', 'password2', '1995-05-15', false);
+
+
+INSERT INTO "Movie" ("title", "genre", "countryoforigin", "productionyear", "suggestedage")
+VALUES
+    ('Movie 1', 'Action', 'USA', '2022-01-01', 'PG'),
+    ('Movie 2', 'Drama', 'UK', '2021-03-15', 'R'),
+    ('Movie 3', 'Comedy', 'Canada', '2023-06-10', 'PG');
+
+INSERT INTO "UserMovies" ("userid", "movieid", "sessiondate", "sessiontime", "sessionpriority")
+VALUES
+    (1, 1, '2023-05-30', '12:00:00', 1),
+    (1, 2, '2023-05-30', '15:30:00', 2),
+    (2, 3, '2023-05-31', '14:00:00', 1);
+
+INSERT INTO "Category" ("categoryname")
+VALUES
+    ('Do piwa'),
+    ('Na randewu'),
+    ('Dupa');
+
+INSERT INTO "MovieCategory" ("usermovieid", "categoryid")
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+INSERT INTO "Director" ("directorfirstname", "directorlastname")
+VALUES
+    ('Christopher', 'Nolan'),
+    ('Quentin', 'Tarantino'),
+    ('Steven', 'Spielberg');
+
+INSERT INTO "Directors" ("directorid", "movieid")
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+INSERT INTO "Producer" ("producername")
+VALUES
+    ('Jerry Bruckheimer'),
+    ('Scott Rudin'),
+    ('Kathleen Kennedy');
+
+INSERT INTO "Producers" ("producerid", "movieid")
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+INSERT INTO "Language" ("languagename")
+VALUES
+    ('English'),
+    ('French'),
+    ('Spanish');
+
+INSERT INTO "Languages" ("languageid", "movieid")
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3);
+
+CREATE OR REPLACE VIEW UserMoviesByCategory AS
+SELECT
+  c.categoryid,
+  um.usermovieid,
+  m.title,
+  m.genre,
+  m.countryoforigin,
+  m.productionyear,
+  m.suggestedage,
+  d.directorfirstname,
+  d.directorlastname,
+  p.producername,
+  l.languagename
+FROM
+  "UserMovies" um
+  INNER JOIN "Movie" m ON um.movieid = m.movieid
+  INNER JOIN "MovieCategory" mc ON um.usermovieid = mc.usermovieid
+  INNER JOIN "Category" c ON mc.categoryid = c.categoryid
+  LEFT JOIN "Directors" dm ON dm.movieid = m.movieid
+  LEFT JOIN "Director" d ON d.directorid = dm.directorid
+  LEFT JOIN "Producers" pm ON pm.movieid = m.movieid
+  LEFT JOIN "Producer" p ON p.producerid = pm.producerid
+  LEFT JOIN "Languages" lm ON lm.movieid = m.movieid
+  LEFT JOIN "Language" l ON l.languageid = lm.languageid;
+
+
+CREATE OR REPLACE VIEW "MovieDetails" AS
+SELECT M.*, L.languagename, D.directorfirstname, D.directorlastname, P.producername
+FROM "Movie" M
+LEFT JOIN "Languages" ML ON M.movieid = ML.movieid
+LEFT JOIN "Language" L ON ML.languageid = L.languageid
+LEFT JOIN "Directors" MD ON M.movieid = MD.movieid
+LEFT JOIN "Director" D ON MD.directorid = D.directorid
+LEFT JOIN "Producers" MP ON M.movieid = MP.movieid
+LEFT JOIN "Producer" P ON MP.producerid = P.producerid;
+
+
+CREATE OR REPLACE VIEW UserMovieDetails AS
+SELECT
+  um.usermovieid,
+  m.title,
+  m.genre,
+  m.countryoforigin,
+  m.productionyear,
+  m.suggestedage,
+  d.directorfirstname,
+  d.directorlastname,
+  p.producername,
+  l.languagename
+FROM
+  "UserMovies" um
+  INNER JOIN "Movie" m ON um.movieid = m.movieid
+  LEFT JOIN "Directors" dm ON dm.movieid = m.movieid
+  LEFT JOIN "Director" d ON d.directorid = dm.directorid
+  LEFT JOIN "Producers" pm ON pm.movieid = m.movieid
+  LEFT JOIN "Producer" p ON p.producerid = pm.producerid
+  LEFT JOIN "Languages" lm ON lm.movieid = m.movieid
+  LEFT JOIN "Language" l ON l.languageid = lm.languageid;
+
+CREATE OR REPLACE VIEW UserCategories AS
+SELECT
+  c.categoryid,
+  c.categoryname,
+  um.userid,
+  u.firstname,
+  u.lastname
+FROM
+  "Category" c
+  INNER JOIN "MovieCategory" mc ON c.categoryid = mc.categoryid
+  INNER JOIN "UserMovies" um ON mc.usermovieid = um.usermovieid
+  INNER JOIN "User" u ON um.userid = u.userid;
+
+
+CREATE OR REPLACE FUNCTION check_duplicate_movie()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM "Movie"
+    WHERE "title" = NEW."title" AND "productionyear" = NEW."productionyear"
+  ) THEN
+    RAISE EXCEPTION 'A movie with the same title and production year already exists.';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER prevent_duplicate_movie
+BEFORE INSERT OR UPDATE ON "Movie"
+FOR EACH ROW
+EXECUTE FUNCTION check_duplicate_movie();
+
+
